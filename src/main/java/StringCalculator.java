b6f28019a4be0d2ee6c1f9d4805272294248b1bc
pnowak86 <p_nowak86@poczta.fm>
2017-06-13 20:19:04 +0200

    second commit

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -2,9 +2,13 @@
 public class StringCalculator {
 
     int add(String numbers){
-if(numbers !=""){
-    return 1;
+    if(numbers !=""){
+        String[] tab = numbers.split(",");
+        int result = Integer.parseInt(tab[0]) + Integer.parseInt(tab[1]);
+        return result;
         }
         return 0;
     }
+
+
 }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -5,42 +5,61 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(JUnitParamsRunner.class)
 public class StringCalculatorTest {
-StringCalculator testedObject;
+    StringCalculator testedObject;
 
     @Test
-    public void testConstructor(){
+    public void testConstructor() {
 
     }
 
     @Before
-    public void setUp(){
+    public void setUp() {
         testedObject = new StringCalculator();
     }
 
     @Test
-    public void shouldReturnZeroForEmptyString(){
-int result = testedObject.add("");
-assertThat(result).isEqualTo(0);
+    public void shouldReturnZeroForEmptyString() {
+        int result = testedObject.add("");
+        assertThat(result).isEqualTo(0);
 
     }
 
-    private Object[][] dataforTestingOutput(){
+    private Object[][] dataforTestingOutput() {
         return new Object[][]{
-                {"",0},
-                {"1",1}
+                {"", 0},
+                {"1", 1},
+                {"1,2", 3},
+                {"7,3", 10},
+                {"120,120", 240}
         };
     }
 
-    @Parameters(method ="dataforTestingOutput")
+    @Parameters(method = "dataforTestingOutput")
 
     @Test
-    public void shouldReturnProperOutputForGivenInput(String input, int expectetResult){
-int result = testedObject.add("1");
-assertThat(result).isEqualTo(1);
+    public void shouldReturnProperOutputForGivenInput(String input, int expectetResult) {
+        int result = testedObject.add("1,2");
+        assertThat(result).isEqualTo(3);
 
     }
 
+    @Parameters(method = "dataforTestingOutput")
+    @Test
+    public void shouldReturnProperOutputForGivenInput2(String input, int expectetResult) {
+        int result = testedObject.add("7,3");
+        assertThat(result).isEqualTo(10);
+
+    }
+
+    @Parameters(method = "dataforTestingOutput")
+    @Test
+    public void shouldReturnProperOutputForGivenInput3(String input, int expectetResult) {
+        int result = testedObject.add("120,120");
+        assertThat(result).isEqualTo(240);
+
+    }
 
 }
